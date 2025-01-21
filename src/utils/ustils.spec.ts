92033d3fc6f81bb48fccdb885f6f682059af2ff2
Dejan Babic <dejan.babic@symphony.is>
2025-01-21 23:15:35 +0100

    chore: test cleanup

--- src/utils/ustils.spec.ts
+++ src/utils/ustils.spec.ts
@@ -1,25 +1,23 @@
 import { verify } from './configuration';
 
-describe('Utils', () => {
-  describe('verify', () => {
-    it('should throw an error if a key is missing in the config', () => {
-      const configuration = {
-        foo: 'bar',
-        baz: undefined,
-      };
+describe('@utils/verify', () => {
+  it('should throw an error if a key is missing in the config', () => {
+    const configuration = {
+      foo: 'bar',
+      baz: undefined,
+    };
 
-      expect(() => verify(configuration)).toThrow('Configuration value [baz] is not set');
-    });
+    expect(() => verify(configuration)).toThrow('Configuration value [baz] is not set');
+  });
 
-    it('should throw an error if a nested key is missing in the config', () => {
-      const configuration = {
-        foo: 'bar',
-        baz: {
-          qux: undefined,
-        },
-      };
+  it('should throw an error if a nested key is missing in the config', () => {
+    const configuration = {
+      foo: 'bar',
+      baz: {
+        qux: undefined,
+      },
+    };
 
-      expect(() => verify(configuration)).toThrow('Configuration value [baz.qux] is not set');
-    });
+    expect(() => verify(configuration)).toThrow('Configuration value [baz.qux] is not set');
   });
 });
