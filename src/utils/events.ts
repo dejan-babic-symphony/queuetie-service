545efdb2f89a6cf1f7efc9d04d522906c49d7ac8
Dejan Babic <dejan.babic@symphony.is>
2025-01-22 20:01:38 +0100

    chore: reorder in file

--- src/utils/events.ts
+++ src/utils/events.ts
@@ -1,19 +1,20 @@
 import { UUID } from 'crypto';
 import { BroadcastEvent, Channel } from '../dispatcher/types';
 
-export const makeServiceErrorEvent = (userId: UUID): BroadcastEvent => {
-  return {
-    scope: 'user',
-    target: userId,
-    event: Channel.SERVICE_ERROR,
-    message: 'QT Jobs dispatch failed',
-  };
-};
 export const makeServiceInfoEvent = (userId: UUID): BroadcastEvent => {
   return {
     scope: 'user',
     target: userId,
     event: Channel.SERVICE_INFO,
     message: 'QT Jobs dispatched',
+  };
+};
+
+export const makeServiceErrorEvent = (userId: UUID): BroadcastEvent => {
+  return {
+    scope: 'user',
+    target: userId,
+    event: Channel.SERVICE_ERROR,
+    message: 'QT Jobs dispatch failed',
   };
 };
